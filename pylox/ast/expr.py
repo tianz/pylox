8f256463f76d326f5928c5ac7dfe47f69eb53a7d
tianz <tianzhou93@gmail.com>
2022-07-25 17:55:09 -0700

    Added and and or operators

--- pylox/ast/expr.py
+++ pylox/ast/expr.py
@@ -23,6 +23,10 @@ class ExprVisitor(ABC):
         pass
 
     @abstractmethod
+    def visit_logical_expr(self, expr):
+        pass
+
+    @abstractmethod
     def visit_unary_expr(self, expr):
         pass
 
@@ -63,6 +67,15 @@ class Literal(Expr):
 
     def accept(self, visitor):
         return visitor.visit_literal_expr(self)
+
+class Logical(Expr):
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor):
+        return visitor.visit_logical_expr(self)
 
 class Unary(Expr):
     def __init__(self, operator, right):
