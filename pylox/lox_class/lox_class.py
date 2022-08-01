c27637774f0646fe615272920de47de0749950d2
Tian Zhou <tianzhou93@gmail.com>
2022-08-01 11:20:38 -0700

    Added class instance

--- pylox/lox_class/lox_class.py
+++ pylox/lox_class/lox_class.py
@@ -1,6 +1,22 @@
-class Class:
+from pylox.function.function import Callable
+
+class Class(Callable):
     def __init__(self, name):
         self.name = name
 
+    def arity(self):
+        return 0
+
+    def call(self, interpreter, arguments):
+        instance = Instance(self)
+        return instance
+
     def __str__(self):
         return self.name
+
+class Instance:
+    def __init__(self, klass):
+        self.klass = klass
+
+    def __str__(self):
+        return self.klass.name + ' instance'
