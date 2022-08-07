5935c8a366696e6c5a54673292f14d7b738b4eb6
Tian Zhou <tianzhou93@gmail.com>
2022-08-07 16:30:06 -0700

    Fixed call tests

--- test/test_call.py
+++ test/test_call.py
@@ -1,35 +1,35 @@
 from textwrap import dedent
 import unittest
 
-from test_util import test_pylox
+from test_util import test_error
 
 class TestCall(unittest.TestCase):
     def test_bool(self):
         expected = dedent("""\
             [line 1] Can only call functions and classes.
             """)
-        test_pylox(self, 'call/bool.lox', expected)
+        test_error(self, 'call/bool.lox', expected, 70)
 
     def test_nil(self):
         expected = dedent("""\
             [line 1] Can only call functions and classes.
             """)
-        test_pylox(self, 'call/nil.lox', expected)
+        test_error(self, 'call/nil.lox', expected, 70)
 
     def test_num(self):
         expected = dedent("""\
             [line 1] Can only call functions and classes.
             """)
-        test_pylox(self, 'call/num.lox', expected)
+        test_error(self, 'call/num.lox', expected, 70)
 
     def test_object(self):
         expected = dedent("""\
             [line 4] Can only call functions and classes.
             """)
-        test_pylox(self, 'call/object.lox', expected)
+        test_error(self, 'call/object.lox', expected, 70)
 
     def test_string(self):
         expected = dedent("""\
             [line 1] Can only call functions and classes.
             """)
-        test_pylox(self, 'call/string.lox', expected)
+        test_error(self, 'call/string.lox', expected, 70)
--- test/test_util.py
+++ test/test_util.py
@@ -9,3 +9,11 @@ def test_pylox(test, input, expected):
         actual = buf.getvalue()
 
         test.assertEqual(expected, actual)
+
+def test_error(test, input, expected, error_code):
+    with io.StringIO() as buf, redirect_stdout(buf):
+        with test.assertRaises(SystemExit) as cm:
+            lox.run_file(f'test/resources/{input}')
+
+        test.assertEquals(cm.exception.code, error_code)
+        test.assertEquals(expected, buf.getvalue())
