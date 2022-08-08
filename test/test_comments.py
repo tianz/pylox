22ed14cca5d9cea4a2614736095666d20d8b7291
Tian Zhou <tianzhou93@gmail.com>
2022-08-08 16:34:18 -0700

    Added comments tests

---
+++ test/test_comments.py
@@ -0,0 +1,25 @@
+from textwrap import dedent
+import unittest
+
+from test_util import test_pylox
+
+class TestBlock(unittest.TestCase):
+    def test_line_at_eof(self):
+        expected = dedent("""\
+            ok
+            """)
+        test_pylox(self, 'comments/line_at_eof.lox', expected)
+
+    def test_only_line_comment(self):
+        expected = ''
+        test_pylox(self, 'comments/only_line_comment.lox', expected)
+
+    def test_only_line_comment_and_line(self):
+        expected = ''
+        test_pylox(self, 'comments/only_line_comment_and_line.lox', expected)
+
+    def test_unicode(self):
+        expected = dedent("""\
+            ok
+            """)
+        test_pylox(self, 'comments/unicode.lox', expected)
